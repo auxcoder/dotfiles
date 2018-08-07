7a0c64310de8a1f3c4d891fd411fff80071bf886
Auxcoder <auxcode@gmail.com>
2018-08-07 11:07:09 -0400

    Add Itermocil to brew install

--- brew.sh
+++ brew.sh
@@ -100,5 +100,8 @@ brew install tree
 brew install webkit2png
 brew install zopfli
 
+# Install tools
+brew install TomAnthony/brews/itermocil
+
 # Remove outdated versions from the cellar.
 brew cleanup
